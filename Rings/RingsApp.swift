28f14f0d8a2443f6a5022e5f41f96b487f8a0125
Chuck Deerinck <cjd9@me.com>
2025-06-06 00:12:46 -0700

    Initial Commit

---
+++ Rings/RingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingsApp.swift
+//  Rings
+//
+//  Created by Chuck Deerinck on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RingsTests/RingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RingsTests.swift
+//  RingsTests
+//
+//  Created by Chuck Deerinck on 6/6/25.
+//
+
+import Testing
+@testable import Rings
+
+struct RingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
